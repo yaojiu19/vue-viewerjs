56106c90d72836133b3efff6c3877358aa40b541
faima <faimaklg@163.com>
2018-09-26 14:48:57 +0800

    开始

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
